e0fa71e9951683f7a0cb11089780108c735caadd
MorganDawe <mdawe@discoverygarden.ca>
2016-06-01 09:13:54 -0300

    Updating site link settings.

--- template.php
+++ template.php
@@ -6,3 +6,12 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728096
  */
+
+/**
+ * Theme preprocessing function for region 'branding'.
+ * @param array $variables
+ *   Region specific variables.
+ */
+function ctda_theme_preprocess_page(&$variables) {
+  $variables['branding_text_link'] = theme_get_setting('uconn_branding_text_link');
+}
